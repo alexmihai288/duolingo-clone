ff6c497b5253149faab23891c8764e9b70f35744
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-07-02 13:31:01 +0300

    chore - learn page layout

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,15 +1,46 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { SidebarItem } from "./SidebarItem";
+import { ClerkLoading, ClerkLoaded, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 
 export const Sidebar = ({ className }: { className?: string }) => {
   return (
     <div
       className={cn(
-        "flex h-full lg:w-[256px] bg-blue-500 lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
         className
       )}
     >
-      Sidebar
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot.png" height={40} width={40} alt="mascot" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem href="/learn" label="Learn" iconSrc="learn.svg" />
+        <SidebarItem
+          href="/leaderboard"
+          label="Leaderboard"
+          iconSrc="leaderboard.svg"
+        />
+        <SidebarItem href="/quests" label="Quests" iconSrc="quests.svg" />
+        <SidebarItem href="/shop" label="Shop" iconSrc="shop.svg" />
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton />
+        </ClerkLoaded>
+      </div>
     </div>
   );
 };
